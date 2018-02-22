dd9c4862dc5cac1c94caf66b7d705302ae418de3
Garrett <Garrettcrg@gmail.com>
2018-02-21 20:17:24 -0600

    Drop-down-menu-added-working

--- media-q/src/Components/Header/Header.js
+++ media-q/src/Components/Header/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import "./Header.css";
 
-function handleClick() {}
+//
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggled: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggled: !prevState.isToggled
+    }));
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="header">
@@ -15,7 +27,8 @@ class Header extends Component {
           <a className="linkas"> TEAM </a>
           <a className="linkas"> CONTACT </a>
         </div>
-        <button id="menuButton" onClick={handleClick}>
+
+        <button id="menuButton" onClick={this.handleClick}>
           MENU
           <div>
             <div className="micon"> </div>
@@ -23,6 +36,16 @@ class Header extends Component {
             <div className="micon"> </div>
           </div>
         </button>
+
+        {this.state.isToggled && (
+          <div id="menu2">
+            <a className=""> SERVICES </a>
+            <a className=""> PORTFOLIO </a>
+            <a className=""> ABOUT </a>
+            <a className=""> TEAM </a>
+            <a className=""> CONTACT </a>
+          </div>
+        )}
       </div>
     );
   }
